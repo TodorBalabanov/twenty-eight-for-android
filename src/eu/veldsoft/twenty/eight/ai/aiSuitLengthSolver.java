df1645a726936cbd7690df21161e69facc90af57
nbu20132014java <nbu20132014java@gmail.com>
2013-11-23 12:43:36 +0200

    Task committed.

---
+++ src/eu/veldsoft/twenty/eight/ai/aiSuitLengthSolver.java
@@ -0,0 +1,264 @@
+/*******************************************************************************
+ *                                                                             *
+ * Twenty-Eight for Android is port of popular Asian card game called Rosanne: *
+ * Twenty-eight (28) <http://sourceforge.net/projects/rosanne/>. Project       *
+ * development is done as NBU Java training course held in Sofia, Bulgaria.    *
+ *                                                                             *
+ * Copyright (C) 2013-2014 by Todor Balabanov  ( tdb@tbsoft.eu )               *
+ *                                                                             *
+ * This program is free software: you can redistribute it and/or modify        *
+ * it under the terms of the GNU General Public License as published by        *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+ *                                                                             *
+ ******************************************************************************/
+
+package eu.veldsoft.twenty.eight.ai;
+
+public class aiSuitLengthSolver {
+	public static final int slLENGTH_MAX = 8;
+
+	public static final int slTOTAL_HANDS = 4;
+
+	public static final int slTOTAL_SUITS = 4;
+
+	public static final int slVACANT = -1;
+
+	private slProblem m_problem;
+
+	private slData m_saved, m_working;
+
+	private int[][] m_played;
+
+	private int m_suit_sum_of_min[] = new int[slTOTAL_SUITS];
+
+	private int m_hand_sum_of_min[] = new int[slTOTAL_HANDS];
+
+	/**
+	 * Disallow copy finalructor/assignment operators
+	 * 
+	 * @param object
+	 * 
+	 * @author INFM042 F___84 Mariya Kostadinova
+	 * @author INFM032 F___06 Rosen Kaplanov
+	 * @author INFM032 F___45 Valentin Popov
+	 */
+	private aiSuitLengthSolver(final aiSuitLengthSolver object) {
+		/*
+		 * Empty body constructor.
+		 */
+	}
+
+	private aiSuitLengthSolver assign(final aiSuitLengthSolver object) {
+		// TODO To be done by INFM032 F___00 Tsvetelina Hristova ...
+		// TODO To be done by INFM032 F___06 Rosen Kaplanov ...
+		// TODO To be done by INFM032 F___90 Svetoslav Slavkov ...
+
+		return (this);
+	}
+
+	private void InitializeWorkingData(slData data) {
+		// TODO To be done by INFM042 F___94 Aleksandar Milev ...
+		// TODO To be done by INFM032 F___46 Nadya Nedyalkova ...
+		// TODO To be done by INFM032 F___88 Ivan Dankinov ...
+	}
+
+	private boolean RecalcCellMax(slData data, int hand, int suit) {
+		// TODO To be done by INFM032 F___88 Ivan Dankinov ...
+		// TODO To be done by INFM042 F___46 Nadya Nedyalkova ...
+		// TODO To be done by INFM042 F___68 Georgi Srebrov ...
+
+		return (false);
+	}
+
+	private boolean SetCell(slData data, int hand, int suit, int suit_length) {
+		// TODO To be done by INFM042 F___45 Valentin Popov ...
+		// TODO To be done by INFM032 F___67 Nevena Sirakova ...
+		// TODO To be done by INFM042 F___39 Shterion Yanev ...
+
+		return (false);
+	}
+
+	private boolean RecalcMaxForImpactedCells(slData data, int hand, int suit) {
+		// TODO To be done by INFM032 F___94 Aleksandar Milev ...
+		// TODO To be done by INFM042 F___90 Svetoslav Slavkov ...
+		// TODO To be done by INFM042 F___68 Nikola Vushkov ...
+
+		return (false);
+	}
+
+	private boolean RecalcMinForImpactedCells(slData data, int hand, int suit) {
+		// TODO To be done by INFM032 F___06 Rosen Kaplanov ...
+		// TODO To be done by INFM032 F___52 Mihail Stankov ...
+		// TODO To be done by INFM032 F___46 Nadya Nedyalkova ...
+
+		return (false);
+	}
+
+	/**
+	 * 
+	 * @param data
+	 * @param hand
+	 * @param suit
+	 * 
+	 * @return
+	 * 
+	 * @author INFM032 F___00 Tsvetelina Hristova
+	 * @author INFM032 F___90 Svetoslav Slavkov
+	 * @author INFM032 F___39 Shterion Yanev
+	 */
+	private boolean RecalcCellMin(slData data, int hand, int suit) {
+		assert (data != null);
+		assert (hand < slTOTAL_HANDS);
+		assert (suit < slTOTAL_SUITS);
+		assert (data.cells[hand][suit].suit_length == slVACANT);
+
+		int i = data.hand_total_length[hand] - data.hand_sum_of_maxs[hand]
+				+ data.cells[hand][suit].max;
+		if (i < 0) {
+			i = 0;
+		}
+
+		int j = data.suit_total_length[suit] - data.suit_sum_of_maxs[suit]
+				+ data.cells[hand][suit].max;
+		if (j < 0) {
+			j = 0;
+		}
+
+		int old_min = data.cells[hand][suit].min;
+		int new_min = Math.max(i, j);
+
+		/*
+		 * The new minimum should be equal to or more than the old minimum
+		 */
+		assert (new_min >= old_min);
+		data.cells[hand][suit].min = new_min;
+
+		/*
+		 * If the new min is different from the old min, correct the sum of
+		 * vacant mins
+		 */
+		if (new_min != old_min) {
+			data.hand_sum_of_vacant_mins[hand] += (new_min - old_min);
+			data.suit_sum_of_vacant_mins[suit] += (new_min - old_min);
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param data
+	 * @return
+	 */
+	private boolean RecalcMinForAllCells(slData data) {
+		RecalcMinForAllCells(data, false);
+
+		return (false);
+	}
+
+	private boolean RecalcMinForAllCells(slData data, boolean changed) {
+		// TODO To be done by INFM042 F___45 Valentin Popov ...
+		// TODO To be done by INFM042 F___14 Petya Atanasova ...
+		// TODO To be done by INFM042 F___06 Rosen Kaplanov ...
+
+		return (false);
+	}
+
+	private boolean RecalcMaxForAllCells(slData data) {
+		// TODO To be done by INFM032 F___67 Nevena Sirakova ...
+		// TODO To be done by INFM032 F___05 Iliya Grozev ...
+		// TODO To be done by INFM032 F___93 Krasimir Chariyski ...
+
+		return (false);
+	}
+
+	private int GenerateRandomFill(int min, int max) {
+		// TODO To be done by INFM032 F___27 Georgi Kostadinov ...
+		// TODO To be done by INFM042 F___84 Mariya Kostadinova ...
+		// TODO To be done by INFM042 F___90 Svetoslav Slavkov ...
+
+		return (0);
+	}
+
+	public static String PrintData(slData data) {
+		// TODO To be done by INFM042 F___93 Krasimir Chariyski ...
+		// TODO To be done by INFM042 F___14 Petya Atanasova ...
+		// TODO To be done by INFM032 F___84 Mariya Kostadinova ...
+
+		return ("");
+	}
+
+	public static String PrintMatrix(int[][] matrix) {
+		// TODO To be done by INFM032 F___00 Tsvetelina Hristova ...
+		// TODO To be done by INFM032 F___48 Georgi Ivanov ...
+		// TODO To be done by INFM032 F___39 Shterion Yanev ...
+
+		return ("");
+	}
+
+	public static void InitializeProblem(slProblem problem) {
+		// TODO To be done by INFM042 F___67 Nevena Sirakova ...
+		// TODO To be done by INFM032 F___88 Ivan Dankinov ...
+		// TODO To be done by INFM042 F___84 Mariya Kostadinova ...
+	}
+
+	public static void InitializePlayed(int[][] played) {
+		// TODO To be done by INFM042 F___81 Marina Rangelova ...
+		// TODO To be done by INFM042 F___88 Ivan Dankinov ...
+		// TODO To be done by INFM032 F___67 Nevena Sirakova ...
+	}
+
+	public aiSuitLengthSolver() {
+		// TODO To be done by INFM042 F___06 Rosen Kaplanov ...
+		// TODO To be done by INFM032 F___88 Ivan Dankinov ...
+		// TODO To be done by INFM042 F___46 Nadya Nedyalkova ...
+	}
+
+	protected void finalize() {
+		// TODO To be done by INFM042 F___94 Aleksandar Milev ...
+		// TODO To be done by INFM032 F___46 Nadya Nedyalkova ...
+		// TODO To be done by INFM042 F___68 Nikola Vushkov ...
+	}
+
+	public boolean SetProblem(slProblem problem, int[][] played) {
+		// TODO To be done by INFM032 F___90 Svetoslav Slavkov ...
+		// TODO To be done by INFM032 F___39 Shterion Yanev ...
+		// TODO To be done by INFM032 F___67 Nevena Sirakova ...
+
+		return (false);
+	}
+
+	public boolean GenerateRandomSolution(int[][] solution) {
+		// TODO To be done by INFM042 F___45 Valentin Popov ...
+		// TODO To be done by INFM032 F___84 Mariya Kostadinova ...
+		// TODO To be done by INFM042 F___68 Georgi Srebrov ...
+
+		return (false);
+	}
+
+	public String PrintSavedData() {
+		// TODO To be done by INFM042 F___46 Nadya Nedyalkova ...
+		// TODO To be done by INFM032 F___47 Kostadin Bulakiev ...
+		// TODO To be done by INFM042 F___27 Georgi Kostadinov ...
+
+		return ("");
+	}
+
+	public String PrintWorkingData() {
+		// TODO To be done by INFM042 F___94 Aleksandar Milev ...
+		// TODO To be done by INFM042 F___48 Georgi Ivanov ...
+		// TODO To be done by INFM032 F___81 Marina Rangelova ...
+
+		return ("");
+	}
+}
---
+++ src/eu/veldsoft/twenty/eight/gg/ggPanel.java
@@ -0,0 +1,204 @@
+/*******************************************************************************
+ *                                                                             *
+ * Twenty-Eight for Android is port of popular Asian card game called Rosanne: *
+ * Twenty-eight (28) <http://sourceforge.net/projects/rosanne/>. Project       *
+ * development is done as NBU Java training course held in Sofia, Bulgaria.    *
+ *                                                                             *
+ * Copyright (C) 2013-2014 by Todor Balabanov  ( tdb@tbsoft.eu )               *
+ *                                                                             *
+ * This program is free software: you can redistribute it and/or modify        *
+ * it under the terms of the GNU General Public License as published by        *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+ *                                                                             *
+ ******************************************************************************/
+
+package eu.veldsoft.twenty.eight.gg;
+
+import eu.veldsoft.twenty.eight.dummy.Globals;
+import eu.veldsoft.twenty.eight.dummy.wxBitmap;
+import eu.veldsoft.twenty.eight.dummy.wxColour;
+import eu.veldsoft.twenty.eight.dummy.wxCoord;
+import eu.veldsoft.twenty.eight.dummy.wxDC;
+import eu.veldsoft.twenty.eight.dummy.wxFont;
+import eu.veldsoft.twenty.eight.dummy.wxPaintEvent;
+import eu.veldsoft.twenty.eight.dummy.wxPoint;
+import eu.veldsoft.twenty.eight.dummy.wxRect;
+import eu.veldsoft.twenty.eight.dummy.wxWindow;
+
+public class ggPanel {
+	private wxBitmap m_back;
+
+	private wxBitmap m_work;
+
+	private boolean m_f_invalid;
+
+	private wxRect m_rect_diff;
+
+	private wxRect m_rect_invalid;
+
+	// Disallow copy finalructor/assignment operators
+	private ggPanel(final ggPanel object) {
+		// TODO To be done by INFM032 F___52 Mihail Stankov ...
+		// TODO To be done by INFM042 F___46 Nadya Nedyalkova ...
+		// TODO To be done by INFM032 F___47 Kostadin Bulakiev ...
+	}
+
+	private ggPanel assign(final ggPanel object) {
+		// TODO To be done by INFM042 F___90 Svetoslav Slavkov ...
+		// TODO To be done by INFM032 F___68 Nikola Vushkov ...
+		// TODO To be done by INFM042 F___00 Tsvetelina Hristova ...
+
+		return (this);
+	}
+
+	public ggPanel(final wxWindow parent) {
+		// TODO To be done by INFM032 F___47 Kostadin Bulakiev ...
+		// TODO To be done by INFM042 F___27 Georgi Kostadinov ...
+		// TODO To be done by INFM032 F___94 Aleksandar Milev ...
+	}
+
+	protected void finalize() {
+		// TODO To be done by INFM032 F___94 Aleksandar Milev ...
+		// TODO To be done by INFM042 F___52 Mihail Stankov ...
+		// TODO To be done by INFM032 F___27 Georgi Kostadinov ...
+	}
+
+	public boolean Size() {
+		// TODO To be done by INFM032 F___56 Daniel Nikolov ...
+		// TODO To be done by INFM042 F___47 Kostadin Bulakiev ...
+		// TODO To be done by INFM042 F___00 Tsvetelina Hristova ...
+
+		return (false);
+	}
+
+	public boolean Paint() {
+		// TODO To be done by INFM042 F___39 Shterion Yanev ...
+		// TODO To be done by INFM032 F___52 Mihail Stankov ...
+		// TODO To be done by INFM032 F___84 Mariya Kostadinova ...
+
+		return (false);
+	}
+
+	public void OnPaint(wxPaintEvent event) {
+		// TODO To be done by INFM032 F___47 Kostadin Bulakiev ...
+		// TODO To be done by INFM042 F___81 Marina Rangelova ...
+		// TODO To be done by INFM032 F___90 Svetoslav Slavkov ...
+	}
+
+	public boolean RefreshScreen() {
+		// TODO To be done by INFM042 F___14 Petya Atanasova ...
+		// TODO To be done by INFM032 F___94 Aleksandar Milev ...
+		// TODO To be done by INFM042 F___68 Nikola Vushkov ...
+
+		return (false);
+	}
+
+	public boolean BlitToBack(wxCoord xdest, wxCoord ydest, wxCoord width,
+			wxCoord height, wxDC source, wxCoord xsrc, wxCoord ysrc) {
+		return (BlitToBack(xdest, ydest, width, height, source, xsrc, ysrc,
+				Globals.wxCOPY, false, null, null));
+	}
+
+	public boolean BlitToBack(wxCoord xdest, wxCoord ydest, wxCoord width,
+			wxCoord height, wxDC source, wxCoord xsrc, wxCoord ysrc,
+			int logicalFunc) {
+		return (BlitToBack(xdest, ydest, width, height, source, xsrc, ysrc,
+				logicalFunc, false, null, null));
+	}
+
+	public boolean BlitToBack(wxCoord xdest, wxCoord ydest, wxCoord width,
+			wxCoord height, wxDC source, wxCoord xsrc, wxCoord ysrc,
+			int logicalFunc, boolean useMask) {
+		return (BlitToBack(xdest, ydest, width, height, source, xsrc, ysrc,
+				logicalFunc, useMask, null, null));
+	}
+
+	public boolean BlitToBack(wxCoord xdest, wxCoord ydest, wxCoord width,
+			wxCoord height, wxDC source, wxCoord xsrc, wxCoord ysrc,
+			int logicalFunc, boolean useMask, wxCoord xsrcMask) {
+		return (BlitToBack(xdest, ydest, width, height, source, xsrc, ysrc,
+				logicalFunc, useMask, xsrcMask, null));
+	}
+
+	public boolean BlitToBack(wxCoord xdest, wxCoord ydest, wxCoord width,
+			wxCoord height, wxDC source, wxCoord xsrc, wxCoord ysrc,
+			int logicalFunc, boolean useMask, wxCoord xsrcMask, wxCoord ysrcMask) {
+		// TODO To be done by INFM042 F___06 Rosen Kaplanov ...
+		// TODO To be done by INFM032 F___67 Nevena Sirakova ...
+		// TODO To be done by INFM042 F___93 Krasimir Chariyski ...
+
+		return (false);
+	}
+
+	public boolean BlitToFront(wxCoord xdest, wxCoord ydest, wxCoord width,
+			wxCoord height, wxDC source, wxCoord xsrc, wxCoord ysrc) {
+		return (BlitToFront(xdest, ydest, width, height, source, xsrc, ysrc,
+				Globals.wxCOPY, false, null, null));
+	}
+
+	public boolean BlitToFront(wxCoord xdest, wxCoord ydest, wxCoord width,
+			wxCoord height, wxDC source, wxCoord xsrc, wxCoord ysrc,
+			int logicalFunc) {
+		return (BlitToFront(xdest, ydest, width, height, source, xsrc, ysrc,
+				logicalFunc, false, null, null));
+	}
+
+	public boolean BlitToFront(wxCoord xdest, wxCoord ydest, wxCoord width,
+			wxCoord height, wxDC source, wxCoord xsrc, wxCoord ysrc,
+			int logicalFunc, boolean useMask) {
+		return (BlitToFront(xdest, ydest, width, height, source, xsrc, ysrc,
+				logicalFunc, useMask, null, null));
+	}
+
+	public boolean BlitToFront(wxCoord xdest, wxCoord ydest, wxCoord width,
+			wxCoord height, wxDC source, wxCoord xsrc, wxCoord ysrc,
+			int logicalFunc, boolean useMask, wxCoord xsrcMask) {
+		return (BlitToFront(xdest, ydest, width, height, source, xsrc, ysrc,
+				logicalFunc, useMask, xsrcMask, null));
+	}
+
+	public boolean BlitToFront(wxCoord xdest, wxCoord ydest, wxCoord width,
+			wxCoord height, wxDC source, wxCoord xsrc, wxCoord ysrc,
+			int logicalFunc, boolean useMask, wxCoord xsrcMask, wxCoord ysrcMask) {
+		// TODO To be done by INFM042 F___68 Georgi Srebrov ...
+		// TODO To be done by INFM032 F___06 Rosen Kaplanov ...
+		// TODO To be done by INFM032 F___00 Tsvetelina Hristova ...
+
+		return (false);
+	}
+
+	public boolean DrawTextOnBack(String text, wxPoint pt) {
+		return (DrawTextOnBack(text, pt, Globals.wxNullColour,
+				Globals.wxNullFont));
+	}
+
+	public boolean DrawTextOnBack(String text, wxPoint pt, wxColour colour) {
+		return (DrawTextOnBack(text, pt, colour, Globals.wxNullFont));
+	}
+
+	public boolean DrawTextOnBack(String text, wxPoint pt, wxColour colour,
+			wxFont font) {
+		// TODO To be done by INFM042 F___27 Georgi Kostadinov ...
+		// TODO To be done by INFM032 F___05 Iliya Grozev ...
+		// TODO To be done by INFM042 F___56 Daniel Nikolov ...
+
+		return (false);
+	}
+
+	public boolean ClearDifference() {
+		// TODO To be done by INFM042 F___88 Ivan Dankinov ...
+		// TODO To be done by INFM042 F___68 Georgi Srebrov ...
+		// TODO To be done by INFM032 F___93 Krasimir Chariyski ...
+
+		return (false);
+	}
+}
