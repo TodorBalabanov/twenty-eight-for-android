dde263c3a61091828b6084ce9ebb6146105142f9
nbu20132014java <nbu20132014java@gmail.com>
2013-12-15 16:29:32 +0200

    One more task.

---
+++ src/eu/veldsoft/twenty/eight/dummy/wxXmlResource.java
@@ -0,0 +1,27 @@
+package eu.veldsoft.twenty.eight.dummy;
+
+public class wxXmlResource {
+
+	public static wxXmlResource Get() {
+		return null;
+	}
+
+	public boolean LoadDialog(wxDialog arg0, Object arg1, String arg2) {
+		return false;
+	}
+
+	public wxBitmap LoadBitmap(String res_name) {
+		return null;
+	}
+
+	public void InitAllHandlers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean Load(String ggCardXrs) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
